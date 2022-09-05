9052d278b5b19a954ca689ffdfff705719a31354
AnthonyLayne <kepheus.ds@gmail.com>
2022-09-05 17:07:57 -0400

    test deply

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Login from "./components/Login";
+//import Login from "./components/Login";
 import Header from "./components/Header";
 import NotesList from "./components/NotesList";
 import NoteEditor from "./components/NoteEditor";
